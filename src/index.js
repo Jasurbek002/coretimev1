2d61c3301dd27d75c25de2fd5ef855b95506ce60
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-08-01 21:59:07 +0500

    push redis remove

--- src/index.js
+++ src/index.js
@@ -40,30 +40,12 @@ var __importDefault =
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
 const postgres_js_1 = require("./lib/postgres.js");
-const fastify_redis_1 = __importDefault(require("fastify-redis"));
 const router_1 = __importDefault(require("./modules/user/router"));
 const router_2 = __importDefault(require("./modules/Device/router"));
 const app = (0, fastify_1.default)({ logger: true });
-app.register(fastify_redis_1.default, { host: "127.0.0.1" });
 app.register(router_1.default);
 app.register(router_2.default);
 app.decorate("sequelize", postgres_js_1.sequelize);
-// fastify-redis
-// app.get("/photos", async (req, rep) => {
-//   const { redis } = app;
-//   redis.get("images"  , async (err, images) => {
-//     if (err) console.log(err);
-//     if (images != null) {
-//       return rep.send(JSON.parse(images));
-//     } else {
-//       const { data } = await axios.get(
-//         "https://jsonplaceholder.typicode.com/photos"
-//       );
-//       redis.set("images", JSON.stringify(data));
-//       return rep.send(data);
-//     }
-//   });
-// });
 
 
 
