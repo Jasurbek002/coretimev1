84a05e4292d590117093171b38342727095f21e6
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-08-01 23:02:21 +0500

    port zbbb

--- src/index.js
+++ src/index.js
@@ -49,8 +49,8 @@ app.decorate("sequelize", postgres_js_1.sequelize);
 
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, (err, address) =>
+
+app.listen({port:Number(process.env.PORT ?? 5001)}, (err, address) =>
   __awaiter(void 0, void 0, void 0, function* () {
     if (err) {
       app.log.error(err);
